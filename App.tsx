2b7297e215fcdb7269b785e91b1ea38515551a43
Jaishree Nishad <jaysh2222@gmail.com>
2024-09-13 12:22:53 +0530

    Style elevated card and create fancy card

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 import FlatCards from './components/FlatCards';
+import ElevatiedCrads from './components/ElevatedCrads';
+import FancyCards from './components/FancyCards';
 // import DraggableDiv from './components/DraggableDiv';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         {/* <DraggableDiv /> */}
 
         <FlatCards />
+        <ElevatiedCrads/>
+        <FancyCards/>
       </ScrollView>
     </SafeAreaView>
   );
